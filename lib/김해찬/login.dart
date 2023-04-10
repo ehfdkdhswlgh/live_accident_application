dfd7059ddc96ba311a987f19452b4da282535060
hae507 <hae507@naver.com>
2023-04-10 20:14:24 +0900

    김해찬

---
+++ lib/김해찬/login.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '로그인 화면',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '로그인 화면'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // 이메일과 비밀번호로 로그인
+      // ...
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: '이메일',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '이메일을 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '비밀번호를 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: _submitForm,
+                  child: Text('로그인'),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.blue,
+                    side: BorderSide(color: Colors.blue),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegisterPage(),
+                      ),
+                    );
+                  },
+                  child: Text('회원가입'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+  TextEditingController();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // 회원가입 처리
+      // ...
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: '이름',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '이름을 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: '이메일',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '이메일을 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '비밀번호를 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _confirmPasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: '비밀번호 확인',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '비밀번호 확인을 입력해주세요.';
+                  }
+                  if (value != _passwordController.text) {
+                    return '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: _submitForm,
+                  child: Text('회원가입'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
