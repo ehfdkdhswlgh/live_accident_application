a1179e042cff26aacd94faa896ce85e24cbb8201
ehfdkdhswlgh <ehfdkdhswlgh@naver.com>
2023-04-10 20:06:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# live_accident_application
+
+live_accident_application
+
+## Getting Started
+
+박지환
---
+++ android/app/src/main/kotlin/com/example/live_accident_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.live_accident_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
