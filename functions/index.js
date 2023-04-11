35589b695a414c22a7c74a25a2e4cd74e15ccd5b
park <durms1125@naver.com>
2023-04-11 20:13:42 +0900

    박지훈 function수정

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,20 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
+// firebase deploy --only "functions:함수이름"
+// 줄바꿈 2줄이상 금지
+// 주석달고 스페이스바 누르고 할말적기
+
+exports.helloWorld5 = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
-
 
 exports.helloJihwan = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello jihwan!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 
-//firebase deploy --only "functions:함수이름"
+exports.helloJihoon = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello jihwan!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
