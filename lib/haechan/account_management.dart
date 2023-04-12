aaeb4eba03f3f2134ad46334728da4f3f14bc5c1
hae507 <hae507@naver.com>
2023-04-12 22:05:47 +0900

    김해찬

---
+++ lib/haechan/account_management.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class AccountManagementScreen extends StatefulWidget {
+  @override
+  _AccountManagementScreenState createState() => _AccountManagementScreenState();
+}
+
+class _AccountManagementScreenState extends State<AccountManagementScreen> {
+  String _nickname = "";
+  String _password = "";
+  List<String> _interestAreas = [];
+  TextEditingController _interestAreaController = TextEditingController();
+  String _errorMessage = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("계정 관리"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _nickname = value;
+                });
+              },
+              decoration: InputDecoration(labelText: "닉네임"),
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+              decoration: InputDecoration(labelText: "비밀번호 수정"),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("관심지역 추가"),
+                      content: TextField(
+                        controller: _interestAreaController,
+                        decoration: InputDecoration(labelText: "관심지역"),
+                        onSubmitted: (value) {
+                          setState(() {
+                            if (_interestAreas.length < 3) {
+                              if (_interestAreas.contains(value)) {
+                                _errorMessage = "이미 있는 지역입니다";
+                              } else {
+                                _interestAreas.add(value);
+                                _errorMessage = "";
+                                Navigator.pop(context);
+                                _interestAreaController.clear();
+                              }
+                            } else {
+                              Navigator.pop(context);
+                            }
+                          });
+                        },
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("취소"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              if (_interestAreas.length < 3) {
+                                if (_interestAreas.contains(_interestAreaController.text)) {
+                                  _errorMessage = "이미 있는 지역입니다";
+                                } else {
+                                  _interestAreas.add(_interestAreaController.text);
+                                  _errorMessage = "";
+                                  Navigator.pop(context);
+                                  _interestAreaController.clear();
+                                }
+                              } else {
+                                Navigator.pop(context);
+                              }
+                            });
+                          },
+                          child: Text("추가"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text("관심지역 추가"),
+            ),
+            SizedBox(height: 16.0),
+            Text("관심지역"),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: _interestAreas.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Text(_interestAreas[index]),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        setState(() {
+                          _interestAreas.removeAt(index);
+                        });
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // 수정된 정보를 서버에 저장하는 로직 추가
+                // _nickname, _password, _interestAreas 변수에 저장된 값을 활용
+                print("닉네임: $_nickname");
+                print("비밀번호: $_password");
+                print("관심지역: $_interestAreas");
+
+                // 서버에 수정된 정보를 전송하고 처리하는 로직을 추가해야 함
+
+                // 수정된 정보를 서버에 성공적으로 전송하면
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("성공"),
+                      content: Text("계정 정보가 성공적으로 저장되었습니다."),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("확인"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text("수정"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/haechan/profile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("회원 정보"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 64.0,
+                  backgroundImage: AssetImage('images/profile1.png'), // 프로필 이미지
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  flex: 10,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "사용자 닉네임", // 닉네임
+                        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start, // 오른쪽 정렬
+                        children: [
+                          Text(
+                            "게시물 200", // 게시물 수
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(width: 8.0),
+                          Text(
+                            "팔로워 100", // 팔로워 수
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(width: 8.0),
+                          Text(
+                            "팔로잉 50", // 팔로잉 수
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // 구독하기 버튼 동작 구현
+                              // ...
+                            },
+                            child: Text("구독하기"),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                              textStyle: TextStyle(fontSize: 12.0), // 버튼 텍스트 스타일
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Divider(thickness: 2.0), // 구분선 추가
+            SizedBox(height: 16.0),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, // 한 줄에 보여줄 게시물 수
+                  crossAxisSpacing: 8.0, // 아이템간의 가로 간격
+                  mainAxisSpacing: 8.0, // 아이템간의 세로 간격
+                ),
+                itemCount: 9, // 게시물 수
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage('images/profile1.png'), // 게시물 이미지
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
